350753b826f1fed2fb55b4b1e76f625a8a16d5b8
anaysharma <anaysharma8791@gmail.com>
2022-10-07 22:34:33 +0530

    added polybar config

---
+++ configs-2.0/firefox/user.js
@@ -0,0 +1,19 @@
+user_pref("dom.image-lazy-loading.enabled", true); // default
+user_pref("browser.sessionstore.restore_on_demand", true); // default
+user_pref("browser.sessionstore.restore_tabs_lazily", true); // default
+user_pref("image.jxl.enabled", true);
+user_pref("layout.css.grid-template-masonry-value.enabled", true);
+user_pref("dom.enable_web_task_scheduling", true);
+user_pref("gfx.offscreencanvas.enabled", true);
+user_pref("layout.css.font-loading-api.workers.enabled", true);
+user_pref("layout.css.animation-composition.enabled", true);
+
+// user_pref("general.smoothScroll", true); // DEFAULT
+// user_pref("mousewheel.default.delta_multiplier_y", 80);
+// user_pref("general.smoothScroll.msdPhysics.enabled", true);
+// user_pref("general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS", 1);
+// user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant", 400);
+// user_pref("general.smoothScroll.msdPhysics.regularSpringConstant", 400);
+// user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaMS", 120);
+// user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaRatio", 0.1);
+// user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant", 1000);
---
+++ configs-2.0/polybar/forest/scripts/powermenu.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+## Author  : Aditya Shakya
+## Mail    : adi1090x@gmail.com
+## Github  : @adi1090x
+## Twitter : @adi1090x
+
+dir="~/.config/polybar/forest/scripts/rofi"
+#uptime=$(uptime -p | sed -e 's/up //g')
+
+rofi_command="rofi -theme $dir/powermenu.rasi"
+
+# Options
+shutdown=
+reboot=
+lock=
+suspend=
+logout= 
+
+# Variable passed to rofi
+options="$suspend\n$logout\n$reboot\n$shutdown"
+
+chosen="$(echo -e "$options" | $rofi_command -p "Uptime: $uptime" -dmenu -selected-row 0)"
+case $chosen in
+    $shutdown) shutdown -h now ;;
+    $reboot) reboot ;;
+    $suspend) systemctl suspend ;;
+    $logout)
+		if [[ "$DESKTOP_SESSION" == "Openbox" ]]; then
+				openbox --exit
+		elif [[ "$DESKTOP_SESSION" == "bspwm" ]]; then
+			bspc quit
+		elif [[ "$DESKTOP_SESSION" == "i3" ]]; then
+			i3-msg exit
+		fi
+	;;
+esac
